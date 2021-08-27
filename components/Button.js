eba780dc381105699493a07fd6a47dc66761b8a2
Jordan Cortes <hello@jordancortes.com>
2021-08-26 23:38:12 -0500

    Add header links

--- components/Button.js
+++ components/Button.js
@@ -1,4 +1,4 @@
-const Button = ({ children, type, className }) => {
+const Button = ({ children, onClick, type, className }) => {
   let l_class =
     "py-2 px-6 bg-primary text-sm text-white font-medium rounded-sm" +
     (className !== undefined ? " " + className : "");
@@ -6,7 +6,7 @@ const Button = ({ children, type, className }) => {
   let l_type = type ? type : "text";
 
   return (
-    <button type={l_type} className={l_class}>
+    <button type={l_type} className={l_class} onClick={onClick}>
       {children}
     </button>
   );
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,32 @@
+import { useRouter } from "next/router";
 import Button from "./Button";
 
 const Header = ({ withButton }) => {
+  const router = useRouter();
+
+  const handleGoToConfirmAssistance = async () => {
+    router.push({ pathname: "/wedding/invitation" });
+  };
+
+  const handleGoToWedding = async () => {
+    router.push({ pathname: "/wedding" });
+  };
+
   return (
     <header className="py-3 border-b flex justify-between">
-      <h1 className="text-primary font-cursive text-4xl">J&amp;V</h1>
-      {withButton ? <Button>Confirmar Asistencia</Button> : ""}
+      <h1
+        onClick={handleGoToWedding}
+        className="text-primary font-cursive text-4xl cursor-pointer"
+      >
+        J&amp;V
+      </h1>
+      {withButton ? (
+        <Button onClick={handleGoToConfirmAssistance}>
+          Confirmar Asistencia
+        </Button>
+      ) : (
+        ""
+      )}
     </header>
   );
 };
