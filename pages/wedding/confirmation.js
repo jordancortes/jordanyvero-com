2701417aac69ad96d352aeb55c24f49ab91a8cda
Jordan Cortes <hello@jordancortes.com>
2021-09-04 09:42:24 -0500

    Fix confirmation static generation

--- pages/wedding/confirmation.js
+++ pages/wedding/confirmation.js
@@ -1,19 +1,31 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import AppContext from "../../util/AppContext";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 
 export default function Confirmation() {
   const router = useRouter();
+  const [assistance, setAssistance] = useState(0);
+  const [isPageReady, setIsPageReady] = useState(false);
   const { invitationQuery } = useContext(AppContext);
+
+  // if invitation is not loaded, we go back to landing
+  useEffect(() => {
+    if (!invitationQuery) {
+      router.push({ pathname: "/" });
+    } else {
+      setAssistance(invitationQuery.assistance);
+      setIsPageReady(true);
+    }
+  }, []);
 
   const handleGoToWedding = () => {
     router.replace({ pathname: "/wedding", query: { code: invitationQuery.code } });
   };
 
-  return (
+  return isPageReady ? (
     <div>
       <Head>
         <title>Boda Jordan&amp;Vero | Confirmaci&oacute;n</title>
@@ -23,7 +35,7 @@ export default function Confirmation() {
       <div className="px-4">
         <div className="flex flex-col items-center text-center space-y-4 pt-4">
           <h2>¡Gracias por confirmar tu asistencia!</h2>
-          {invitationQuery.assistance !== 0 ? (
+          {assistance !== 0 ? (
             <div className="space-y-4">
               <p>
                 Si necesitas hacer un cambio aun lo puedes hacer y te invitamos a ver el resto de la
@@ -38,5 +50,7 @@ export default function Confirmation() {
         </div>
       </div>
     </div>
+  ) : (
+    ""
   );
 }
