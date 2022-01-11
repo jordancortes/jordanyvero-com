939668b9ddfa3762aada1399580f0e738ec13bf3
Jordan Cortes <apps@jordancortes.com>
2022-01-11 10:03:08 -0600

    Fix arrayFrom function to include array length
    When converting an object to an array, it creates an empty array, so isArray always comes back true

--- util/AppContext.js
+++ util/AppContext.js
@@ -12,7 +12,7 @@ export const AppContextProvider = ({ children }) => {
   const prepareInivitationInput = (form) => {
     let guests;
 
-    if (Array.isArray(Array.from(form.guests))) {
+    if (Array.from(form.guests).length > 0) {
       guests = Array.from(form.guests).map((g, idx) => {
         return {
           name: form.name[idx].value,
